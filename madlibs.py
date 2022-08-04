3a686f8c5668667f5fad99d6ec80c8ac57825f32
recon317 <106416469+recon317@users.noreply.github.com>
2022-08-03 22:37:15 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,45 @@
+from logging import BufferingFormatter
+import re
+from pathlib import Path
+
+madLibsFile = open('C:/Users/rybbo/Desktop/Code/Python/automateTheBoringStuff/practiceProjects/madLibs/madLibs.txt')
+
+madLibsContent = madLibsFile.read()
+
+madList = re.split(pattern= r"[\.\n ]", string = madLibsContent)
+
+madLibsFile.close()
+
+for i in range(len(madList)):
+    if madList[i] == 'ADJECTIVE':
+        print('Please enter an adjective:')
+        newAdj = input()
+        madList[i] = newAdj
+
+    if madList[i] == 'NOUN':
+        print('Please enter a noun:')
+        newNoun = input()
+        madList[i] = newNoun
+    
+    if madList[i] == 'VERB':
+        print('Please enter a verb:')
+        newVerb = input()
+        madList[i] = newVerb
+    
+    if madList[i] == '':
+        madList[i] = '.'
+
+
+#print(madList)
+
+madLib = ' '.join(madList)
+
+finalMadLib = madLib.replace(' .', '.')
+
+print(finalMadLib)
+
+newMadLibsFile = open('C:/Users/rybbo/Desktop/Code/Python/automateTheBoringStuff/practiceProjects/madLibs/newMadLibs.txt', 'w')
+
+newMadLibsFile.write(finalMadLib)
+
+newMadLibsFile.close()
